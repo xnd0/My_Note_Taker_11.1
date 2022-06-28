ef64aded3e22498c24922227a19dc2f1aaaeed39
xndcmd <xndcmd@gmail.com>
2022-06-28 11:31:54 -0700

    installed packages and added server.js file

---
+++ server.js
@@ -0,0 +1,11 @@
+// --------------------- //
+// ----- server.js ----- //
+// --------------------- //
+
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
