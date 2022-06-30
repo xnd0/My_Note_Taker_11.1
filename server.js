dbf15238bdc086c386b145932181119ee1aa8e16
xndcmd <xndcmd@gmail.com>
2022-06-30 16:22:59 -0700

    added wildcard route + listener to spin up local server

--- server.js
+++ server.js
@@ -28,3 +28,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+
+
+
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
