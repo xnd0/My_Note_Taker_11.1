4e3e79d1321c60db7e47e940ab55287db892667b
xndcmd <xndcmd@gmail.com>
2022-06-30 17:08:04 -0700

    adding POST route for adding notes. In progress

--- routes/notes.js
+++ routes/notes.js
@@ -3,6 +3,35 @@
 // ----------------------------- //
 
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+
+
+// POST Route for a new note
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { username, topic, tip } = req.body;
+  
+    if (req.body) {
+      const newTip = {
+        username,
+        tip,
+        topic,
+        tip_id: uuidv4(),
+      };
+  
+      readAndAppend(newTip, './db/db.json');
+      res.json(`Tip added successfully 🚀`);
+    } else {
+      res.error('Error in adding tip');
+    }
+  });
 
 
 
