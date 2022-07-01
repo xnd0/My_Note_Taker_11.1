2161fba39be459c1ca6ef72d0aa59fa97fcfbb07
xndcmd <xndcmd@gmail.com>
2022-06-30 18:18:31 -0700

    added DELETE Route for removing specific note functionality

--- routes/notes.js
+++ routes/notes.js
@@ -17,6 +17,25 @@ notes.get('/', (req, res) => {
   });
 
 
+// DELETE Route for a specific tip
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all Notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
+
+
+
 // POST Route for a new note
 notes.post('/', (req, res) => {
     console.log(req.body);
